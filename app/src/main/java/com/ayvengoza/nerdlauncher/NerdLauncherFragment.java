e07f9c0cdab7651199a9dce8520cf7cfff8b0b5f
ayvengoza <ayvengoza@gmail.com>
2017-10-26 11:18:07 +0300

    add recyclerview

---
+++ app/src/main/java/com/ayvengoza/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,37 @@
+package com.ayvengoza.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ang on 26.10.17.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        NerdLauncherFragment fragment = new NerdLauncherFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/ayvengoza/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.ayvengoza.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ang on 26.10.17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment getFragment();
+
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = getFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
